8fe3161ca7458dbfda02a1e9cba217f9542bf9cd
Hopansh Gahlot <51720190+hopansh@users.noreply.github.com>
2020-12-13 23:33:09 +0530

    Color persistence

--- Tango Browser/Properties/Settings.Designer.cs
+++ Tango Browser/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Tango_Browser.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,6 +25,7 @@ namespace Tango_Browser.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("WhiteSmoke")]
         public global::System.Drawing.Color FormBackGround {
             get {
                 return ((global::System.Drawing.Color)(this["FormBackGround"]));
